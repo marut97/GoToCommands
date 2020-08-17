e4532af0c552d3ff12837f8b131d9ee5c5277b90
marut97 <marut1997@gmail.com>
2020-08-17 18:00:34 +0530

    Initial commit. Dummy commands available in tools menu and right click

---
+++ ViewCodeExtension/ViewCodeExtension/ViewCodeCommand.cs
@@ -0,0 +1,189 @@
+﻿//using System;
+//using System.ComponentModel.Design;
+//using System.Globalization;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using Microsoft.VisualStudio.Shell;
+//using Microsoft.VisualStudio.Shell.Interop;
+//using Task = System.Threading.Tasks.Task;
+
+//namespace ViewCodeExtension
+//{
+//    /// <summary>
+//    /// Command handler
+//    /// </summary>
+//    internal sealed class ViewCodeCommand
+//    {
+//        /// <summary>
+//        /// Command ID.
+//        /// </summary>
+//        public const int CommandId = 0x0100;
+
+//        /// <summary>
+//        /// Command menu group (command set GUID).
+//        /// </summary>
+//        public static readonly Guid CommandSet = new Guid("ee1f93c0-e081-4ef4-8891-06343538812c");
+
+//        /// <summary>
+//        /// VS Package that provides this command, not null.
+//        /// </summary>
+//        private readonly AsyncPackage package;
+
+//        /// <summary>
+//        /// Initializes a new instance of the <see cref="ViewCodeCommand"/> class.
+//        /// Adds our command handlers for menu (commands must exist in the command table file)
+//        /// </summary>
+//        /// <param name="package">Owner package, not null.</param>
+//        /// <param name="commandService">Command service to add command to, not null.</param>
+//        private ViewCodeCommand(AsyncPackage package, OleMenuCommandService commandService)
+//        {
+//            this.package = package ?? throw new ArgumentNullException(nameof(package));
+//            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+//            var menuCommandID = new CommandID(CommandSet, CommandId);
+//            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+//            commandService.AddCommand(menuItem);
+//        }
+
+//        /// <summary>
+//        /// Gets the instance of the command.
+//        /// </summary>
+//        public static ViewCodeCommand Instance
+//        {
+//            get;
+//            private set;
+//        }
+
+//        /// <summary>
+//        /// Gets the service provider from the owner package.
+//        /// </summary>
+//        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+//        {
+//            get
+//            {
+//                return this.package;
+//            }
+//        }
+
+//        /// <summary>
+//        /// Initializes the singleton instance of the command.
+//        /// </summary>
+//        /// <param name="package">Owner package, not null.</param>
+//        public static async Task InitializeAsync(AsyncPackage package)
+//        {
+//            // Switch to the main thread - the call to AddCommand in ViewCodeCommand's constructor requires
+//            // the UI thread.
+//            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+//            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+//            Instance = new ViewCodeCommand(package, commandService);
+//        }
+
+//        /// <summary>
+//        /// This function is the callback used to execute the command when the menu item is clicked.
+//        /// See the constructor to see how the menu item is associated with this function using
+//        /// OleMenuCommandService service and MenuCommand class.
+//        /// </summary>
+//        /// <param name="sender">Event sender.</param>
+//        /// <param name="e">Event args.</param>
+//        private void Execute(object sender, EventArgs e)
+//        {
+//            ThreadHelper.ThrowIfNotOnUIThread();
+//            string message;
+//            string title = "ViewCodeCommand";
+//            EnvDTE.DTE dte;
+//            EnvDTE.Document activeDocument;
+
+//            dte = (EnvDTE.DTE)this.ServiceProvider.GetService(typeof(EnvDTE.DTE));
+//            activeDocument = dte.ActiveDocument;
+//            message = $"Called on {activeDocument.FullName}";
+
+//            // Show a message box to prove we were here
+//            VsShellUtilities.ShowMessageBox(
+//                this.package,
+//                message,
+//                title,
+//                OLEMSGICON.OLEMSGICON_INFO,
+//                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+//                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+//        }
+//    }
+//}
+
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace ViewCodeExtension
+{
+    internal sealed class ViewCodeCommand
+    {
+        public const int CommandId = 0x0100;
+
+        public static readonly Guid CommandSet = new Guid("0c1acc31-15ac-417c-86b2-eefdc669e8bf");
+
+        private readonly Package package;
+
+        private ViewCodeCommand(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        public static ViewCodeCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        public static void Initialize(Package package)
+        {
+            Instance = new ViewCodeCommand(package);
+        }
+
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            string message;
+            string title = "ViewCodeCommand";
+            EnvDTE.DTE dte;
+            EnvDTE.Document activeDocument;
+
+            dte = (EnvDTE.DTE)ServiceProvider.GetService(typeof(EnvDTE.DTE));
+            activeDocument = dte.ActiveDocument;
+            message = $"Called on {activeDocument.FullName}";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                this.ServiceProvider,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
